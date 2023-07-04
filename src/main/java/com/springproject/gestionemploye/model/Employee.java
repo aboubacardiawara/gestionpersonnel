b711c7c5454058ca1ddf0dfb1890bb3542a349d2
Aboubacar Diawara <aboubacardiawara909@gmail.com>
2023-07-04 17:49:25 +0200

    employee model

---
+++ src/main/java/com/springproject/gestionemploye/model/Employee.java
@@ -0,0 +1,14 @@
+package com.springproject.gestionemploye.model;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String mail;
+    private String password;
+
+}
