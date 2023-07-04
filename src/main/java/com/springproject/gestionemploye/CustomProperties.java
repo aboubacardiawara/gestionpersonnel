f8499eb5b30a0761ba8a1253de240e034d2df442
Aboubacar Diawara <aboubacardiawara909@gmail.com>
2023-07-04 17:44:34 +0200

    configuration du projet (props + props custom)

---
+++ src/main/java/com/springproject/gestionemploye/CustomProperties.java
@@ -0,0 +1,12 @@
+package com.springproject.gestionemploye;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix="com.springproject.gestionemploye")
+public class CustomProperties {
+    private String apiUrl;
+}
