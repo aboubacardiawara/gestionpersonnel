ac3a4d82151c6b12fd780091468e644ffff5aa0d
Aboubacar Diawara <aboubacardiawara909@gmail.com>
2023-07-04 17:19:41 +0200

    init commit

---
+++ src/main/java/com/springproject/gestionemploye/GestionemployeApplication.java
@@ -0,0 +1,13 @@
+package com.springproject.gestionemploye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionemployeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionemployeApplication.class, args);
+	}
+
+}
