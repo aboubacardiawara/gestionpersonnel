bf00a181b28efb89666c5074974247c8611964f8
aboubacardiawara <aboubacardiawara909@gmail.com>
2023-07-06 04:35:30 +0200

    feeding view with data from controller

--- src/main/java/com/springproject/gestionemploye/controller/EmployeeController.java
+++ src/main/java/com/springproject/gestionemploye/controller/EmployeeController.java
@@ -1,18 +1,26 @@
 package com.springproject.gestionemploye.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.springproject.gestionemploye.model.Employee;
 import com.springproject.gestionemploye.service.EmployeeService;
 
 @Controller
 public class EmployeeController {
+
     @Autowired
     private EmployeeService employeeService;
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        Iterable<Employee> employees = employeeService.getEmployees();
+        System.out.println("Employees: " + employees);
+        model.addAttribute("employees", employees);
         return "home";
     }
 
