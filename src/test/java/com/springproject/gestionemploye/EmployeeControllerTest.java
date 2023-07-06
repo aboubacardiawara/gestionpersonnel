e553b3ad836589592321b34147de5ab520d7272a
aboubacardiawara <aboubacardiawara909@gmail.com>
2023-07-06 04:57:58 +0200

    adding some tests

---
+++ src/test/java/com/springproject/gestionemploye/EmployeeControllerTest.java
@@ -0,0 +1,34 @@
+package com.springproject.gestionemploye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import com.springproject.gestionemploye.controller.EmployeeController;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testGetHome() throws Exception {
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(containsString("Laurent")));
+    }
+
+}
--- src/test/java/com/springproject/gestionemploye/GestionemployeApplicationTests.java
+++ src/test/java/com/springproject/gestionemploye/GestionemployeApplicationTests.java
@@ -1,7 +1,12 @@
 package com.springproject.gestionemploye;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.springproject.gestionemploye.service.EmployeeService;
 
 @SpringBootTest
 class GestionemployeApplicationTests {
